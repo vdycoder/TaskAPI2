4d1173f4feef67f9ef9a7e208ff1248771a5d995
dmitry_v <pub.dv@ya.ru>
2017-03-14 03:57:41 +0300

    part5 worked

--- TaskApp/views.py
+++ TaskApp/views.py
@@ -7,9 +7,10 @@ from .Serializers import TaskSerializer
 class TaskViewSet(viewsets.ModelViewSet):
 	queryset = Task.objects.all()
 	serializer_class = TaskSerializer
-	filter_backends = (filters.DjangoFilterBackend,filters.OrderingFilter)
+	filter_backends = (filters.DjangoFilterBackend,filters.OrderingFilter,filters.SearchFilter,)
 	filter_fields = ('completed',)
 	ordering = ('-date_created',)
+	search_fields = ('^task_name',)
 
 # class DueTaskViewSet(viewsets.ModelViewSet):
 # 	queryset = Task.objects.all().order_by('-date_created').filter(completed=False)
